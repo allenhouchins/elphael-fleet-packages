7b29efa299aa16b7f51938fcf015a7979fffb440
Allen Houchins <allenhouchins@mac.com>
2024-09-29 23:37:44 -0500

    Update create_google_chrome_package.sh

--- .github/scripts/create_google_chrome_package.sh
+++ .github/scripts/create_google_chrome_package.sh
@@ -4,10 +4,10 @@
 PACKAGE_URL="https://github.com/autopkg/autopkg/releases/download/v2.7.3/autopkg-2.7.3.pkg"
 DOWNLOAD_PATH="/tmp/autopkg-2.7.3.pkg"
 USER_HOME=$(eval echo ~$SUDO_USER)
-AUTOPKG_CACHE_PATH="${USER_HOME}/Library/AutoPkg/Cache/com.github.autopkg.pkg.googlechrome/"
+# AUTOPKG_CACHE_PATH="${USER_HOME}/Library/AutoPkg/Cache/com.github.autopkg.pkg.googlechrome/"
 # REPO_URL="xxx" ## Uncomment if running locally
 # PACKAGE_AUTOMATION_TOKEN="xxx" ## Uncomment if running locally
-PACKAGE_FILE=$(ls "${AUTOPKG_CACHE_PATH}"/GoogleChrome*.pkg 2>/dev/null)
+PACKAGE_FILE=$(ls "${USER_HOME}"/Library/AutoPkg/Cache/com.github.autopkg.pkg.googlechrome/GoogleChrome*.pkg 2>/dev/null)
 # REPO_OWNER="xxx" ## Uncomment if running locally
 # REPO_NAME="" ## Uncomment if running locally
 
